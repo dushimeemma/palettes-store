902ed5da082f1035f4a1a4a472c105a9547112e9
dushimeemma <dushimeemma@gmail.com>
2025-02-17 20:54:22 +0200

    feature(home): landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-import Header from "./components/layout/Header";
+import Header from "@/app/components/layout/widgets/Header";
 
 const poppins = Poppins({
   subsets: ["latin"],
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import HomePage from "@/app/components/layout/pages/Home";
+
 export default function Home() {
   return (
     <div className="flex h-screen w-screen items-center justify-center font-poppins">
-      WIP
+      <HomePage />
     </div>
   );
 }
